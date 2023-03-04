b5df3500280b37972cc77211f62a050c1e755a33
Jikku Jose <jikku@storybrain.com>
2023-03-04 23:32:20 +0530

    Pack nvim config

--- install.zsh
+++ install.zsh
@@ -2,4 +2,4 @@ ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.config/nvim/
-ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/nvim/ ~/.config/nvim/
